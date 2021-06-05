b6f4e7b07cb95e85cdc5196edac506227d543f2d
Amal Assem <amalassem21@gmail.com>
2021-06-05 18:34:45 +0200

    Initial Commit

--- Script/Script.js
+++ Script/Script.js
@@ -1,96 +1,101 @@
-/*
- *First check validation 
- *if it true 
- *
- */
-
-
-
 var arr = [];
-
+var flag2 = false;
 document.getElementById('add').onclick = function() {
-    if (check_validation()) {
-        if (GetData() !== null)
-            PushData();
-        document.getElementById("noData").innerHTML = "";
-        //user object
-        var userdata = {
-            Id: document.getElementById("id").value,
+    if (!flag2) {
+        check_Validation();
+    } else {
+        document.getElementById('NotFound').innerText = " ";
+        document.getElementById("DisplayInTable").innertext = " ";
+        var user = {
+            ID: document.getElementById("id").value,
             FullName: document.getElementById("name").value,
             Email: document.getElementById("email").value,
             Telepone: document.getElementById("tel").value,
             Age: document.getElementById("age").value
         }
-        arr.push(userdata);
-        //store user data in localStorage
-        localStorage.setItem("User_Data", JSON.stringify(arr));
-        alert("Record of data is Saved");
+        localStorage.setItem("User_Data", JSON.stringify(user));
+        var getdata = JSON.parse(localStorage.getItem("User_Data"));
+        //console.log(getdata);
+        /*check id*/
+        if (arr.length == 0) {
+            // for the first time where array is empty
+            arr.push(getdata);
+            alert("Record of data is Saved");
+        } else {
+            var flag;
+            for (var j = 0; j < arr.length; j++) {
+                if (document.getElementById("id").value != arr[j].ID) {
+                    flag = true;
+                } else {
+                    flag = false
+                }
+            }
+            if (flag) {
+                arr.push(getdata);
+                alert("Record of data is Saved.");
+            } else {
+                alert("Id must be unique.");
+                console.log("Id must be unique.");
+            }
+        }
+        flag2 = false;
     }
 }
-
-function PushData() {
+document.getElementById('display').onclick = function() {
     if (arr.length == 0) {
-        for (i = 0; i < GetData().length; i++) {
-            arr.push(GetData()[i]);
+        document.getElementById('NotFound').innerText = "There is no data found to display!!!!";
+    } else {
+        document.getElementById('NotFound').innerText = " ";
+        document.getElementById('DisplayInTable').innerHTML = "<tr> <th>ID</th> <th>User Name</th> <th>Email</th><th>Telephone</th> <th>Age</th> </tr>";
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("DisplayInTable").innerHTML += "<tr> <td>" + arr[i].ID + " </td>   <td>" + arr[i].FullName + "  </td>  <td>" + arr[i].Email + "  </td> <td>" + arr[i].Telepone + " </td>  <td>" + arr[i].Age + "  </td>  </tr>";
         }
     }
 }
 
-function GetData() {
-    var getdata = JSON.parse(localStorage.getItem("User_Data"));
-    return getdata;
-}
-
-function check_validation() {
+function check_Validation() {
     if (document.getElementById("id").value == "") {
-        alert("ID don't allow to be empty and must be unique number.");
-        return false;
+        alert("Id must be filled");
+        // flag2 = false;
     } else {
-        for (i = 0; i < arr.length; i++) {
-            if (document.getElementById("id").value == arr[i].Id) {
-                alert("ID must be Unique!!!!");
-                return false;
+        if (document.getElementById("name").value == "") {
+            alert("Name must be filled");
+            flag2 = false;
+        } else {
+            if (document.getElementById("email").value == "") {
+                alert("Email must be fill and must follow the mail pattern");
+                flag2 = false;
+            } else {
+                if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.com$/g.test(document.getElementById("email").value))) {
+                    alert("Email must follow the mail pattern");
+                    flag2 = false;
+                } else {
+                    if (document.getElementById("tel").value == "") {
+                        alert("Phone must be filled");
+                        flag2 = false;
+                    } else {
+                        if (!(/^[0-9]{4}-[0-9]{3}-[0-9]{4}$/g.test(document.getElementById("tel").value))) {
+                            alert("Phone must  follow pattern [0000-000-0000].");
+                            flag2 = false;
+                        } else {
+                            if (document.getElementById("age").value == "") {
+                                alert("Age must be filled and must be less or equal 45.");
+                                flag2 = false;
+                            } else {
+                                if (document.getElementById("age").value > 45) {
+                                    alert("Age must be less or equal 45");
+                                    flag2 = false;
+                                } else {
+                                    flag2 = true;
+                                }
+
+
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-    if (document.getElementById("name").value == "") {
-        alert("input don't allow to be empty and must start with alphabet letter.");
-        return false;
-    }
-    if (document.getElementById("email").value == "") {
-        alert("input don't allow to be empty and must be liked email pattern [name@example.com].");
-        return false;
-    }
-    if (document.getElementById("tel").value == "") {
-        alert("input don't allow to be empty and must be in pattern of[0000-000-0000");
-        return false;
-    }
-    if (document.getElementById("age").value == "") {
-        alert("input don't allow to be empty and must be number from 1 to 45");
-        return false;
-    } else if (age.value < 1 || age.value > 45) {
-        alert("Age must be number from 1 to 45");
-        return false;
-
-    }
-    return true;
-}
-document.getElementById("display").onclick = function() {
-    if (GetData() == null) {
-        document.getElementById("noData").innerHTML = "There is no data found to display!!!!";
-    } else {
-        document.getElementById("DisplayTable").innerHTML =
-            "<tr> <th>ID</th> <th>User Name</th> <th>Email</th><th>Telephone</th> <th>Age</th> </tr>";
-        for (i = 0; i < GetData().length; i++) {
-            document.getElementById("DisplayTable").innerHTML += "<tr> <td>" + GetData()[i].Id + " </td>   <td>" + GetData()[i].FullName + "  </td>  <td>" + GetData()[i].Email + "  </td> <td>" + GetData()[i].Telepone + " </td>  <td>" + GetData()[i].Age + "  </td>  </tr>";
-        }
-    }
-}
-document.getElementById("Clear").onclick = function() {
-    document.getElementById("DisplayTable").innerHTML = "";
-}
-document.getElementById("Remove").onclick = function() {
-    window.localStorage.removeItem('User_Data');
-    document.getElementById("Clear").onclick();
 
 }
